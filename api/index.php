6fbbc9cab35978e251026f9601bb812751d567f9
baday4121 <baday4121@gmail.com>
2026-08-19 19:24:18 +0700

    Debug Vercel runtime error

--- api/index.php
+++ api/index.php
@@ -1,3 +1,18 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    http_response_code(500);
+
+    echo '<pre>';
+    echo 'ERROR: ' . $e->getMessage() . "\n\n";
+    echo 'FILE: ' . $e->getFile() . "\n";
+    echo 'LINE: ' . $e->getLine() . "\n\n";
+    echo $e->getTraceAsString();
+    echo '</pre>';
+}
